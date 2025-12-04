51be6cd3a9534c8286722d14692754326c41e242
Renato <105134815+renPaiva-dev@users.noreply.github.com>
2025-12-03 21:29:44 -0300

    Delete Downloads/renato-pos-system directory

--- Downloads/renato-pos-system/src/main/java/com/renato/configuration/JwtConstant.java
+++
@@ -1,6 +0,0 @@
-package com.renato.configuration;
-
-public class JwtConstant {
-    public static final String JWT_SECRET = "dsjhfbsdjfbsdjhfbsdjhfbsdsduhfusd,dsfhsbdnhjfbsdhjfbds,dsfnsdhfnhsjdfnhjsd,dsfjhnsdnhfjy";
-    public static final String JWT_HEADER = "Authorization";
-}
